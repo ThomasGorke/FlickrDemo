9e3116313cbb043aae3d4b3c98866026f9395399
Thomas Gorke <>
2023-08-24 22:26:52 +0200

    added dao

--- app/src/main/java/at/thomasgorke/photofeed/data/local/PhotoDao.kt
+++ app/src/main/java/at/thomasgorke/photofeed/data/local/PhotoDao.kt
@@ -1,9 +1,20 @@
 package at.thomasgorke.photofeed.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import at.thomasgorke.photofeed.data.local.entity.PhotoEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PhotoDao {
 
+    @Insert
+    suspend fun insertPhotos(vararg photos: PhotoEntity)
 
+    @Query("SELECT * FROM photos")
+    fun getPhotos(): Flow<PhotoEntity>
+
+    @Query("DELETE FROM photos")
+    suspend fun deleteAll()
 }
